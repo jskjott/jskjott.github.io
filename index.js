234041fc4f008227429dc71bb5f2166e191b0b71
jskjott <jonathanskjott@Jonathans-MacBook-Pro-3.local>
2019-06-17 11:52:59 +0200

    add new project pages

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var vue = new Vue({
+let vue = new Vue({
   el: "#vue",
   data: {
     mainArea: 'projects',
     items: [
-      { 
-        title: "Digital Historiography",
-        text: "Tools for investigating the pas",
-        image: "img/digital_historiogaphy.png",
-        class: "card visual",
-        link: "assets/pdfs/The Spatial History of a History Journal.pdf",
+    { 
+        title: "Service Design for Participatory Science",
+        text: "Prototyping social dynamics in participatory research.",
+        image: "img/service_design_research.png",
+        class: "card design",
+        link: "dclabs.html",
     },{ 
         title: "Auto Rickshaw Documentary Project",
         text: "Rickshaw driver families in Hydarabad",
@@ -16,17 +16,17 @@ var vue = new Vue({
         class: "card storytelling",
         link: "tuktuk.html",
     },{ 
-        title: "Josenjok of Daerim-dong",
-        text: "A walking tour about a Chinese Korean communit",
-        image: "img/daerim_dong.png",
-        class: "card storytelling",
-        link: "Joseonjok.html",
+        title: "Portable Typewriter",
+        text: "A first experiment in electronics.",
+        image: "img/portable_typewriter.png",
+        class: "card design",
+        link: "assets/pdfs/Receipt_Printer_Typewriter.pdf",
     },{ 
-        title: "Metro Global",
-        text: "The contentious history of metro system",
-        image: "img/metro_global.png",
-        class: "card storytelling",
-        link: "metro_global.html",
+        title: "Digital Historiography",
+        text: "Tools for investigating the past.",
+        image: "img/digital_historiogaphy.png",
+        class: "card visual",
+        link: "history.html",
     },{ 
         title: "Spatially Conscious News",
         text: "An alternative news aggregato",
@@ -34,35 +34,35 @@ var vue = new Vue({
         class: "card visual",
         link: "spatial_news_interface/index.html",
     },{ 
-        title: "Portable Typewriter",
-        text: "A first experiment in electronic",
-        image: "img/portable_typewriter.png",
-        class: "card design",
-        link: "assets/pdfs/Receipt_Printer_Typewriter.pdf",
+        title: "Josenjok of Daerim-dong",
+        text: "A walking tour about a Chinese Korean community.",
+        image: "img/daerim_dong.png",
+        class: "card storytelling",
+        link: "Joseonjok.html",
+    },{ 
+        title: "Metro Global",
+        text: "The contentious history of metro system.",
+        image: "img/metro_global.png",
+        class: "card storytelling",
+        link: "metro_global.html",
     },{ 
         title: "Becoming an Online Learner",
-        text: "Mooc introducing refugees to online learning",
+        text: "MOOC introducing refugees to online learning.",
         image: "img/kiron_mooc.png",
         class: "card design",
         link: "kiron.html",
     },{ 
         title: "Color in Film",
-        text: "A model generating color palettes for feature films",
+        text: "A model generating color palettes for feature films.",
         image: "img/modelling_color_in_film.png",
         class: "card visual",
         link: "film_colour_simulation/index.html",
     },{ 
         title: "Mother",
-        text: "A homage to family and heritage",
+        text: "A homage to family and heritage.",
         image: "img/mother_video_art.png",
         class: "card visual",
         link: "mother/index.html",
-    },{ 
-        title: "Service Design for Participatory Science",
-        text: "Prototyping social dynamics in participatory research",
-        image: "img/service_design_research.png",
-        class: "card design",
-        link: "dclabs.html",
     },{ 
         title: "Slitscan experiments",
         text: "An exploration of manipulating time in video.",
@@ -72,6 +72,15 @@ var vue = new Vue({
     }
     ]
   }
+})
+
+let header = new Vue({
+    el: '#header',
+    computed: {
+        renderProjectMenu: function(){
+            return document.documentElement.clientWidth > 410 ? true : false
+        }
+    }
 })
 
 if (typeof HTMLCollection.prototype.forEach === "undefined") {
